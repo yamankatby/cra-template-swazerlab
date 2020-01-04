c41c097289884e4f613fad49e1ac33e8661a0e08
Yaman KATBY <m.yaman.katby@gmail.com>
2020-01-04 16:40:40 +0300

    📦 Convert to create-react-app template

--- template/src/layout/404.tsx
+++ template/src/layout/404.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const NotFound = () => (
 	<div className='crp'>
 		<h2>404 Not Found</h2>
-		<p>Either you're requesting an invalid URI or the resource in question doesn't exist<br /> (ex: no such user).
+		<p>
+			Either you're requesting an invalid URI or the resource in question doesn't exist<br /> (ex: no such user).
 		</p>
 	</div>
 );
